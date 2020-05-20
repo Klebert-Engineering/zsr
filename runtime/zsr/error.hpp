e57bce943d836ff8d74b25d4357ee4e2107d7820
Johannes Wolf <j.wolf@klebert-engineering.de>
2020-05-20 14:00:08 +0200

    build: Silence MSVC warnings about unexported type

--- runtime/zsr/error.hpp
+++ runtime/zsr/error.hpp
@@ -5,9 +5,10 @@
 #include <string>
 
 #ifdef _MSC_VER
-/* Disable warning about inheriting from unexported class */
+/* Disable warning about inheriting from/using unexported class */
 #pragma warning(push)
 #pragma warning(disable:4275)
+#pragma warning(disable:4251)
 #endif
 
 namespace zsr {
