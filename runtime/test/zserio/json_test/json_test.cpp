c45549834ea6e3f9c1ac1ebb6ea29cb61f399d2e
Johannes Wolf <j.wolf@klebert-engineering.de>
2021-07-09 09:25:48 +0200

    test: Remove json tests

--- runtime/test/zserio/json_test/json_test.cpp
+++ runtime/test/zserio/json_test/json_test.cpp
@@ -10,52 +10,6 @@ namespace {
 
 PKG;
 
-TEST_CASE("empty_object", "[JsonTest::empty_object]")
-{
-    speedyj::Stream ss;
-    ss << speedyj::Object << speedyj::End;
-
-    REQUIRE(ss.str() == R"({})");
-}
-
-TEST_CASE("empty_array", "[JsonTest::empty_array]")
-{
-    speedyj::Stream ss;
-    ss << speedyj::Array << speedyj::End;
-
-    REQUIRE(ss.str() == R"([])");
-}
-
-TEST_CASE("key_value_pair", "[JsonTest::key_value_pair]")
-{
-    speedyj::Stream ss;
-    ss << speedyj::Object
-       << "key" << "value"
-       << speedyj::End;
-
-    REQUIRE(ss.str() == R"({"key":"value"})");
-}
-
-TEST_CASE("json_string_escaping", "[JsonTest::json_string_escaping]")
-{
-    speedyj::Stream ss;
-    ss << speedyj::Object
-       << "\"hello\"" << "\\world\\"
-       << speedyj::End;
-
-    REQUIRE(ss.str() == "{\"\\\"hello\\\"\":\"\\\\world\\\\\"}");
-}
-
-TEST_CASE("json_uchar_string_escaping", "[JsonTest::json_uchar_string_escaping]")
-{
-    speedyj::Stream ss;
-    ss << speedyj::Object
-       << "\"hello\x02\"" << "\\\xC2\xB9\\"
-       << speedyj::End;
-
-    REQUIRE(ss.str() == "{\"\\\"hello\\u0002\\\"\":\"\\\\\\u00C2\\u00B9\\\\\"}");
-}
-
 TEST_CASE("serialize_meta_subtype", "[JsonTest::serialize_meta_subtype]")
 {
     speedyj::Stream ss;
